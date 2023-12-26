07e88e86388619dfdd1b0b9a45568b2cbae2129d
Chia20-1 <chiachunlim97@gmail.com>
2023-12-26 03:56:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const busStopInput = document.getElementById("busStopId");
+const arrivalInfo = document.getElementById("arrivalInfo")
+
+async function fetchBusArrival(busStopId) {
+  try {
+    const response = await fetch(`https://sg-bus-arrivals.sigma-schoolsc1.repl.co/?id=${busStopId}`);
+    if(response.ok){
+      const data = await response.json();
+      return data;
+    } else{
+      throw new Error("Error fetching bus arrival data.")
+    }
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+}
+
+function formatArrivalData(arrivalData){
+  const buses = arrivalData.services;
+  const formattedData = [];
+  for(const bus of buses) {
+    const arrivalTimeString = `${bus.next_bus_mins} mins(s)`;
+    formattedData.push(`
+    <div>
+      <strong>Bus ${bus.bus_no}</strong>: ${arrivalTimeString}
+    </div>
+    `)
+  }
+  return formattedData.join("")
+}
+
+function displayBusArrival(busStopId) {
+  arrivalInfo.innerHTML = "Loading...";
+  fetchBusArrival(busStopId)
+    .then((arrivalData)=>{
+      const formattedArrivalData = formatArrivalData(arrivalData);
+      arrivalInfo.innerHTML = formattedArrivalData;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function getBusTiming(){
+  const busStopId = busStopInput.value;
+  displayBusArrival(busStopId)
+}
